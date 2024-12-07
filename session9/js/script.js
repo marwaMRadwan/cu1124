e4570024f178f31a6bf0b6f8d7adc17d6ce79c34
marwaMRadwan <44086877+marwaMRadwan@users.noreply.github.com>
2024-12-07 21:59:13 +0200

    s9

---
+++ session9/js/script.js
@@ -0,0 +1,189 @@
+// comment
+
+/*
+comment
+ */
+
+// Variables
+// var a = 5
+// var a = "marwa"
+// var a = true
+// var a = 5.5
+// var a = null
+// var a = undefined
+// let a = 5
+// a = "marwa"
+// const PI = 3.14
+
+//w = true // var w =5
+
+// console.log(w)
+
+// document.write(w)
+// alert(w)
+
+// let name = prompt("enter your name")
+// document.write(name)
+// let arr = [ 5, "a", true, 7.5 ]
+
+// let obj = {
+//     name: "marwa",
+//     age: 40
+// }
+// console.log(typeof obj)
+
+// let name = prompt("name")
+// alert( "welcome " + name)
+
+
+// let name = "marwa", age = 40 , job = "developer"
+
+// alert(`welcome\nuser name is ${name}\nuser age is ${age}\nwork as ${job} \\`)
+
+
+//Math Op.    + - * /  **   %
+//  ()  unary  * /  + -
+
+// let x = -1
+// x = -x
+// console.log(x)
+
+
+// increment decrement
+// let x = 5
+// console.log(x++)  //6
+// ++x  // 7
+
+
+//assignment = += -= *= /= %=
+// let x = 1
+// x += 5  // x = x+5
+// x *= 5
+
+// comp logical
+// == != === !== > < >= <=
+// && || !  
+// 5 == '5'    // true
+// 5 != '5'    // false
+
+// 5 === '5'   // false
+// 5 !== '5'   // true
+
+// 5 > 7 //false
+// 5 >=5  //true
+
+// 5 < 5 //false
+// 5<=5  //true
+
+// name=="marwa" || age == 50  //true
+
+// !false  
+
+// !(age==50)
+
+// let age = +prompt("enter your age")
+
+// age === 40 ? alert("yes") : alert("No")
+
+
+// (50 == 50 && 5>3) ? console.log("yes"):console.log("no");
+
+// decisions
+
+// if else
+// const log = console.log
+// let name = prompt("name")
+// if(name == "marwa") log("yes")
+// else log("no")
+// let grade = prompt("enter your grade")
+
+// if(grade>90) log("A")
+// else if(grade>80) log("B")
+// else if(grade>70) log("C")
+// else if(grade>60) log("D")
+// else log("F")
+// let m = prompt("month number")
+// switch(m){
+//     case "1": 
+//         month = "Jan"; break;
+//     case "2":
+//         month = "Feb"; break;
+//     case "3":
+//         month = "Mar";
+//         break;
+//     default:
+//         month = "Invalid month"
+// }
+// console.log(month)
+
+// let yearOfBirth = +prompt("enter year of birth")
+// console.log(yearOfBirth%12)
+// if( typeof yearOfBirth == "number" && yearOfBirth>0){
+//     switch(yearOfBirth%12){
+//         case 0: console.log("rat0"); break;
+//         case 1: console.log("rat1"); break;
+//         case 2: console.log("rat2"); break;
+//         case 3: console.log("rat3"); break;
+//         case 4: console.log("rat4"); break;
+//         case 5: console.log("rat5"); break;
+//         case 6: console.log("rat6"); break;
+//         case 7: console.log("rat7"); break;
+//         case 8: console.log("rat8"); break;
+//         case 9: console.log("rat9"); break;
+//         case 10: console.log("rat10"); break;
+//         case 11: console.log("rat11"); break; 
+//     }
+// }
+// else{
+//     console.log("invalid input")
+// }
+
+
+//loops
+// while for do while
+// let ch
+// do{
+//     ch = prompt(`1- chicken\n2-meat\n3-fish\n0- exit`)
+// }while(ch!=0)
+
+
+// let i = 1
+// while(i<=10){
+//     console.log(i)
+//     i+=2
+// }
+
+// let c = prompt(`1- chicken\n2-meat\n3-fish\n0- exit`)
+// while(c!=0){
+//     c = prompt(`1- chicken\n2-meat\n3-fish\n0- exit`)
+// }
+
+// for(let j=0; j<=10; j+=2){
+//     console.log(j)
+// }
+
+// for(;;){
+//     let ch=prompt('')
+//     if(ch==0) break
+// }
+
+// for(;true;){}
+
+// for(let i=1; i<10; i++){
+//     if(i==5) continue
+//     console.log(i)
+// }
+
+for( i=0 ; i<=50 ; i+=2 ){
+    // if(!(i%2)) console.log(i)
+    console.log(i)
+}
+
+
+
+
+
+
+
+
+
