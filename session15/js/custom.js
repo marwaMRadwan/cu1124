e3237e2904e8d36f41cd194727b05f03cd1caa54
marwaMRadwan <44086877+marwaMRadwan@users.noreply.github.com>
2024-12-21 20:25:51 +0200

    s15

--- session15/js/custom.js
+++ session15/js/custom.js
@@ -1,18 +1,30 @@
 const data = []
-const inputs = ["title", "name", "age", "job"]
+const inputs = ["title", "name", "age", "job", "gender", "status"]
+
 const myForm = document.querySelector("form")
 const tbody = document.querySelector("tbody")
+
 const createUserObj = (myForm) => {
     const user = { id: Date.now() }
+    let formStatus = true
     inputs.forEach( inputData => {
         try{
-            user[inputData] = myForm[inputData].value 
+            if(inputData=="status")
+                user[inputData] = myForm[inputData].checked || "false"
+            else{
+                if(myForm[inputData].value == "") {
+                    myForm[inputData].nextSibling.nextSibling.innerHTML=`<p class="alert alert-danger">${inputData} is empty</p>`
+                    formStatus = false
+                }
+                user[inputData] = myForm[inputData].value 
+            }
         }
         catch(err){
             user[inputData] = ""
         }
     })
-    return user
+
+    return {user:user, formStatus:formStatus}
 }
 
 const createMyOwnEle = (parent, ele, txt=null) => {
@@ -25,18 +37,16 @@ const drawUser = (obj) => {
     const row = createMyOwnEle(tbody, "tr")
     createMyOwnEle( row, "td", obj.id)
     inputs.forEach(inp => createMyOwnEle( row, "td", obj[inp]))
-    // createMyOwnEle( row, "td", obj.title)
-    // createMyOwnEle( row, "td", obj.name)
-    // createMyOwnEle( row, "td", obj.age)
-    // createMyOwnEle( row, "td", obj.job)
 }
 
 myForm.addEventListener("submit", function(e){
     e.preventDefault()
-    const user = createUserObj(myForm)
-    drawUser(user)
-    data.push(user)
-    this.reset()
+    const {user, formStatus} = createUserObj(this)
+    if(formStatus){
+        drawUser(user)
+        // data.push(user)
+        this.reset()
+    }
 })
 
 
